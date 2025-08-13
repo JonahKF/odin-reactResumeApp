38c007e30089cbccd7e3ace0f1590e1eee386c1f
jonah <jonahkf@gmail.com>
2025-08-13 10:40:53 +0900

    Added job title, location custom inputs. Adjusted height on root element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -66,7 +66,7 @@ function App() {
     }));
   };
 
-  console.log({ person });
+  // console.log({ person });
 
   return (
     <>
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -5,18 +5,36 @@ function GeneralInfo({ personData, onChange }) {
   return (
     <div className="general-info info-card">
       <h2>General Information</h2>
+
+      <div className="name-input">
+        <CustomInput
+          field="firstName"
+          value={personData.firstName}
+          onChange={onChange}
+        />
+
+        <CustomInput
+          field="lastName"
+          value={personData.lastName}
+          onChange={onChange}
+        />
+      </div>
+
       <CustomInput
-        field="firstName"
-        value={personData.firstName}
+        field="jobTitle"
+        value={personData.jobTitle}
         onChange={onChange}
       />
-      <CustomInput
-        field="lastName"
-        value={personData.lastName}
-        onChange={onChange}
-      />
-      <CustomInput field="email" value={personData.email} onChange={onChange} />
+
       <CustomInput field="phone" value={personData.phone} onChange={onChange} />
+
+      <CustomInput field="email" value={personData.email} onChange={onChange} />
+
+      <CustomInput
+        field="location"
+        value={personData.location}
+        onChange={onChange}
+      />
     </div>
   );
 }
